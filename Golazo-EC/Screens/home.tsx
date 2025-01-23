01cd81e473f94a03c5343b4334efae722ffdc896
Anderson Josue <anderjosue456@gmail.com>
2025-01-23 14:13:46 -0500

    Sign In Google

--- Golazo-EC/Screens/home.tsx
+++ Golazo-EC/Screens/home.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet, Alert } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, Button, StyleSheet, Alert, Image } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App'; // Asegúrate de ajustar la ruta si es necesario
 import { getAuth, signOut } from 'firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
   const auth = getAuth();
+  const [userInfo, setUserInfo] = React.useState<{ email: string | null; picture?: string; email_verified?: boolean; name?: string } | null>(null);
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const data = await AsyncStorage.getItem('@user');
+        if (data) {
+          setUserInfo(JSON.parse(data));
+        }
+      } catch (error) {
+        console.error('Failed to fetch user info:', error);
+      }
+    };
+    fetchUserInfo();
+  }, []);
 
   const handleLogout = async () => {
     try {
-      await signOut(auth); // Cerrar sesión con Firebase
+      // Sign out from Firebase
+      await signOut(auth);
+
+      // Sign out from Google
+      // await GoogleSignin.signOut();
+
+      // Clear local storage
+      await AsyncStorage.removeItem('@user');
+      setUserInfo(null);
+
       Alert.alert('Logout Successful', 'You have been logged out.');
       navigation.replace('Login'); // Redirigir al usuario a la pantalla de Login
     } catch (error) {
@@ -38,6 +64,17 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+  },
+  card: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  email: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 16,
   },
 });
 
--- Golazo-EC/Screens/login.tsx
+++ Golazo-EC/Screens/login.tsx
@@ -5,13 +5,28 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { firebaseConfig } from '../firebase-config';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App'; // Asegúrate de ajustar la ruta si es necesario
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+WebBrowser.maybeCompleteAuthSession();
 type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+
+type UserInfo = {
+  email: string;
+  email_verified: boolean;
+  name: string;
+};
 
 const LoginScreen: React.FC<Props> = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [userInfo, setUserInfo] = React.useState<UserInfo | null>(null);
+  const [request, response, promptAsyn] = Google.useAuthRequest({
+    webClientId: '523537409192-qsdp95lcn7nblht5a3iqas7gisr2ivpq.apps.googleusercontent.com',
+    androidClientId:'523537409192-9kt6g6lsml82n20radf5npi04a4u4rqq.apps.googleusercontent.com'
+  });
+  
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
 
@@ -38,6 +53,49 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
       });
   };
 
+  // Incio google
+  // web: 523537409192-qsdp95lcn7nblht5a3iqas7gisr2ivpq.apps.googleusercontent.com
+  // android: 523537409192-9kt6g6lsml82n20radf5npi04a4u4rqq.apps.googleusercontent.com
+
+  React.useEffect(() => {
+    handleSignInWithGoogle();
+  }, [response])
+
+  async function handleSignInWithGoogle() {
+    const user = await getLocalUser();
+    if(!user) {
+      if (response?.type === 'success') {
+        if (response.authentication) {
+          getUserInfo(response.authentication.accessToken);
+          navigation.replace('Home');
+        }
+      }
+    }else{  
+      setUserInfo(user);
+    }
+  }
+  
+
+  const getLocalUser = async () => {
+    const data = await AsyncStorage.getItem('@user');
+    if(!data) return null;
+    return JSON.parse(data);
+  }
+
+  const getUserInfo = async (accessToken: string) => {
+    if(!accessToken) return;
+    try {
+      const response = await fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json', {
+        headers: { Authorization: `Bearer ${accessToken}` }
+      });
+      const userInfo = await response.json();
+      await AsyncStorage.setItem('@user', JSON.stringify(userInfo));
+      setUserInfo(userInfo);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+      
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
@@ -57,6 +115,7 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
       />
       <Button title="Login" onPress={handleSignIn} />
       <Button title="Create Account" onPress={handleCreateAccount} />
+      <Button title="Login with Google"  disabled={!request} onPress={() => {promptAsyn(); }} />
     </View>
   );
 };
